542fe1fa732758dca27bb9ee3cdc15cc706e9322
gnghii1509 <gianghi15092007@gmail.com>
2026-02-05 09:35:49 +0700

    ote

--- bai2.c
+++ bai2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Tôn Thất Gia Nghi]
+ * MSSV:      [PS48181]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 2: TÌM GIÁ TRỊ LỚN NHẤT VÀ NHỎ NHẤT TRONG MẢNG 
@@ -9,4 +9,38 @@
 //  Output: Xuất ra màn hình giá trị nhỏ nhất và lớn nhất của mảng
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+int main() {
+    int n, arr[100];
+    int max, min;
 
+    // Nhập số lượng phần tử của mảng
+    printf("Nhập số lượng phần tử của mảng: ");
+    scanf("%d", &n);
+
+    // Nhập các phần tử của mảng
+    printf("Nhập các phần tử của mảng:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Khởi tạo giá trị lớn nhất và nhỏ nhất
+    max = arr[0];
+    min = arr[0];
+
+    // Tìm giá trị lớn nhất và nhỏ nhất trong mảng
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    // Xuất kết quả
+    printf("Giá trị lớn nhất trong mảng là: %d\n", max);
+    printf("Giá trị nhỏ nhất trong mảng là: %d\n", min);
+
+    return 0;
+}
